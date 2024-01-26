c2ed11ae724ab6e96d2dc43e9dbd73a22f0bc66c
Daniyar Mukhamedgaliyev <daniyar.m2003@gmail.com>
2024-01-26 01:30:36 -0500

    Completed working version of fighter position progression graph

--- src/main/java/com/cft/components/impl/CFTSimplePositionGraphGenerator.java
+++ src/main/java/com/cft/components/impl/CFTSimplePositionGraphGenerator.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class CFTSimplePositionGraphGenerator implements ICFTPositionGraphGenerator {
 
     private static final int GRAPH_WIDTH = 800, GRAPH_HEIGHT = 600;
-    private static final int GRAPH_HORIZONTAL_PADDING = 40, GRAPH_VERTICAL_PADDING = 20;
+    private static final int GRAPH_HORIZONTAL_PADDING = 60, GRAPH_VERTICAL_PADDING = 50;
 
     private static final Color GRAPH_BORDER_COLOR = new Color(0, 0, 0);
     private static final Stroke GRAPH_BORDER_STROKE = new BasicStroke(5.0f);
@@ -32,17 +32,20 @@ public class CFTSimplePositionGraphGenerator implements ICFTPositionGraphGenerat
     private static final Color GRAPH_LINE_COLOR = new Color(0, 128, 255);
     private static final Stroke GRAPH_LINE_STROKE = new BasicStroke(3.0f);
 
+    private static final Stroke GRAPH_TICK_MARK_STROKE = new BasicStroke(1.5f);
+    private static final int GRAPH_TICK_MARK_SIZE = 10;
+
+    private static final int MAX_NUM_HORIZONTAL_TICK_MARKS = 5, MAX_NUM_VERTICAL_TICK_MARKS = 10;
+
     private static final String GRAPH_FILE_TYPE = "png";
 
     @Autowired
     private CFTEventSnapshotEntryRepo snapshotEntryRepo;
 
-    private void drawGraph(@NonNull Graphics2D imageGraphics, @NonNull Fighter fighter, List<CFTEventSnapshotEntry> snapshotEntries) {
+    private void drawLineGraph(@NonNull Graphics2D imageGraphics, List<CFTEventSnapshotEntry> snapshotEntries, int maxPosition) {
 
         // This entity has to exist if this function is being called
-        int maxPosition = this.snapshotEntryRepo.findFirstByOrderByPositionDesc().getPosition();
 
-        // Draw line graph
         imageGraphics.setColor(GRAPH_LINE_COLOR);
         imageGraphics.setStroke(GRAPH_LINE_STROKE);
 
@@ -50,8 +53,8 @@ public class CFTSimplePositionGraphGenerator implements ICFTPositionGraphGenerat
         int graphDrawableHeight = GRAPH_HEIGHT - 2 * GRAPH_VERTICAL_PADDING;
 
         for(int i = 0; i < snapshotEntries.size() - 1; i++) {
-            float graphLineRelLeftX = (float)graphDrawableWidth * (float)i / (float)snapshotEntries.size();
-            float graphLineRelRightX = (float)graphDrawableWidth * (float)(i + 1) / (float)snapshotEntries.size();
+            float graphLineRelLeftX = (float)graphDrawableWidth * (float)i / (float)(snapshotEntries.size() - 1);
+            float graphLineRelRightX = (float)graphDrawableWidth * (float)(i + 1) / (float)(snapshotEntries.size() - 1);
 
             float graphLineRelLeftY = (float)graphDrawableHeight * (float)snapshotEntries.get(i).getPosition() / (float)maxPosition;
             float graphLineRelRightY = (float)graphDrawableHeight * (float)snapshotEntries.get(i + 1).getPosition() / (float)maxPosition;
@@ -59,8 +62,9 @@ public class CFTSimplePositionGraphGenerator implements ICFTPositionGraphGenerat
             imageGraphics.drawLine(GRAPH_HORIZONTAL_PADDING + (int)graphLineRelLeftX, GRAPH_VERTICAL_PADDING + (int)graphLineRelLeftY,
                     GRAPH_HORIZONTAL_PADDING + (int)graphLineRelRightX, GRAPH_VERTICAL_PADDING + (int)graphLineRelRightY);
         }
+    }
 
-        // Draw graph border
+    private void drawGraphBorder(@NonNull Graphics2D imageGraphics) {
         imageGraphics.setColor(GRAPH_BORDER_COLOR);
         imageGraphics.setStroke(GRAPH_BORDER_STROKE);
 
@@ -69,6 +73,74 @@ public class CFTSimplePositionGraphGenerator implements ICFTPositionGraphGenerat
 
         imageGraphics.drawLine(GRAPH_HORIZONTAL_PADDING, GRAPH_HEIGHT - GRAPH_VERTICAL_PADDING,
                 GRAPH_WIDTH - GRAPH_HORIZONTAL_PADDING, GRAPH_HEIGHT - GRAPH_VERTICAL_PADDING);
+    }
+
+    private void drawHorizontalAxis(@NonNull Graphics2D imageGraphics, List<CFTEventSnapshotEntry> snapshotEntries) {
+        imageGraphics.setColor(GRAPH_BORDER_COLOR);
+        imageGraphics.setStroke(GRAPH_TICK_MARK_STROKE);
+
+        int numTickMarks = Math.min(snapshotEntries.size(), MAX_NUM_HORIZONTAL_TICK_MARKS);
+
+        for(int i = 0; i < numTickMarks; i++) {
+            float lerpVal = (float)i / (float)(numTickMarks - 1);
+            int snapshotIndex = (int)(lerpVal * (snapshotEntries.size() - 1));
+
+            String eventName = snapshotEntries.get(snapshotIndex).getSnapshot().getEvent().getName();
+
+            int tickMarkLeftLimit = GRAPH_HORIZONTAL_PADDING;
+            int tickMarkRightLimit = GRAPH_WIDTH - GRAPH_HORIZONTAL_PADDING;
+
+            int tickMarkX = (int)((float)tickMarkLeftLimit + lerpVal * (tickMarkRightLimit - tickMarkLeftLimit));
+
+            imageGraphics.drawLine(tickMarkX, GRAPH_HEIGHT - GRAPH_VERTICAL_PADDING,
+                    tickMarkX, GRAPH_HEIGHT - GRAPH_VERTICAL_PADDING + GRAPH_TICK_MARK_SIZE);
+
+            FontMetrics fontMetrics = imageGraphics.getFontMetrics();
+
+            int fontHeight = fontMetrics.getHeight();
+
+            imageGraphics.drawString(eventName, tickMarkX, GRAPH_HEIGHT - GRAPH_VERTICAL_PADDING + GRAPH_TICK_MARK_SIZE + fontHeight);
+        }
+    }
+
+    private void drawVerticalTickMarks(@NonNull Graphics2D imageGraphics, int maxPosition) {
+        int positionIncreaseRate = maxPosition / MAX_NUM_VERTICAL_TICK_MARKS;
+
+        // If more than the max number of ticks can be drawn, increment the fighter position increase rate
+        int remainingPositions = maxPosition - positionIncreaseRate * MAX_NUM_VERTICAL_TICK_MARKS;
+        if(remainingPositions >= positionIncreaseRate) {
+            positionIncreaseRate++;
+        }
+
+        for(int position = 0; position <= maxPosition; position += positionIncreaseRate) {
+            float lerpVal = (float)position / (float)maxPosition;
+
+            int tickMarkTopLimit = GRAPH_VERTICAL_PADDING;
+            int tickMarkBottomLimit = GRAPH_HEIGHT - GRAPH_VERTICAL_PADDING;
+
+            int tickMarkY = (int)((float)tickMarkTopLimit + lerpVal * (tickMarkBottomLimit - tickMarkTopLimit));
+
+            imageGraphics.drawLine(GRAPH_HORIZONTAL_PADDING - GRAPH_TICK_MARK_SIZE, tickMarkY,
+                    GRAPH_HORIZONTAL_PADDING, tickMarkY);
+
+            String positionText = position != 0 ? String.valueOf(position) : "C";
+            FontMetrics fontMetrics = imageGraphics.getFontMetrics();
+
+            int positionTextWidth = fontMetrics.stringWidth(positionText);
+
+            imageGraphics.drawString(positionText, GRAPH_HORIZONTAL_PADDING - GRAPH_TICK_MARK_SIZE - positionTextWidth,
+                    tickMarkY);
+        }
+    }
+
+    private void drawGraph(@NonNull Graphics2D imageGraphics, List<CFTEventSnapshotEntry> snapshotEntries) {
+        // Add one to make bottom position visible on graph (rather than covered by border)
+        int maxPosition = this.snapshotEntryRepo.findFirstByOrderByPositionDesc().getPosition() + 1;
+
+        this.drawLineGraph(imageGraphics, snapshotEntries, maxPosition);
+        this.drawGraphBorder(imageGraphics);
+        this.drawHorizontalAxis(imageGraphics, snapshotEntries);
+        this.drawVerticalTickMarks(imageGraphics, maxPosition);
     }
 
     @Override
@@ -80,7 +152,7 @@ public class CFTSimplePositionGraphGenerator implements ICFTPositionGraphGenerat
         Graphics2D imageGraphics = image.createGraphics();
 
         if(snapshotEntries.size() >= 2) {
-            this.drawGraph(imageGraphics, fighter, snapshotEntries);
+            this.drawGraph(imageGraphics, snapshotEntries);
         }
 
         ByteArrayInputStream graphImageInStream;
